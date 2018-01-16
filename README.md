6d466afbb3fd59dc8144d6269b343d9dd4754afd
unknown <source.changs@gmail.com>
2018-01-17 02:13:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# hujackman_android
--- app/src/main/java/human/hujackman/SplashActivity.java
+++ app/src/main/java/human/hujackman/SplashActivity.java
@@ -33,7 +33,7 @@ public class SplashActivity extends AppCompatActivity {
         // SET 글로벌 환경변수
         AppConf conf = GlobalVal.getInstance().getAppConf();
         //conf.setSvrUrl("http://192.168.0.10:3000/");
-        conf.setSvrUrl("http://172.16.1.254:3000/");
+        conf.setSvrUrl("http://192.168.10.152:3000/");
 
         return true;
     }
